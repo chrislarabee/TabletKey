11d2a0b5d2b945a7f665ed2d66cc31a74edb19ce
cwlarabee <chris.larabee9@gmail.com>
2020-05-25 18:52:37 -0500

    Added the ability to save binding sets.

--- TabletKeyApp.py
+++ TabletKeyApp.py
@@ -1,11 +1,11 @@
+import json
 import time
 
 import pyautogui as pg
 from kivy.app import App
 from kivy.properties import (
-    ListProperty, BooleanProperty, ObjectProperty, StringProperty)
+    ListProperty, BooleanProperty, ObjectProperty, StringProperty, DictProperty)
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 
@@ -15,9 +15,17 @@ class Keybind(Button):
     bound_keys = ListProperty([])
     tab_back = BooleanProperty(False)
 
-    def setup(self, binding: str):
+    def output(self):
+        return dict(
+            bound_keys=self.bound_keys,
+            tab_back=self.tab_back
+        )
+
+    def setup(self, binding: str, **options):
         self.bound_key_text = binding
         self.bound_keys = binding.split('+')
+        self.tab_back = options.get('tab_back', False)
+        self.text = '+'.join([b.capitalize() for b in self.bound_keys])
 
     def send(self):
         pg.hotkey('alt', 'tab')
@@ -40,21 +48,48 @@ class NewBindDialogue(AnchorLayout):
         self.parent.remove_widget(self)
 
     def commit(self):
-        new_btn = Keybind()
-        new_btn.setup(self.bind_text)
-        self.parent.btn_layout.add_widget(new_btn)
+        self.parent.update_keybind(self.bind_text, **self.__dict__)
+        self.parent.remove_widget(self)
+
+
+class SaveBindSet(AnchorLayout):
+    name_input = ObjectProperty(None)
+
+    def cancel(self):
+        self.parent.remove_widget(self)
+
+    def commit(self):
+        self.parent.save(self.name_input.text)
         self.parent.remove_widget(self)
 
 
 class AppFrame(FloatLayout):
     btn_layout = ObjectProperty(None)
+    keybindings = DictProperty({})
 
     def new_keybind_dialogue(self):
         nbd = NewBindDialogue()
         self.add_widget(nbd)
 
-    def new_keybind(self, bind_text, **options):
-        pass
+    def new_save_dialogue(self):
+        sbs = SaveBindSet()
+        self.add_widget(sbs)
+
+    def update_keybind(self, bind_text: str, **options):
+        if self.keybindings.get(bind_text):
+            self.keybindings[bind_text].setup(bind_text, **options)
+        else:
+            new_btn = Keybind()
+            new_btn.setup(bind_text, **options)
+            self.keybindings[bind_text] = new_btn
+            self.btn_layout.add_widget(new_btn)
+
+    def save(self, file_name: str):
+        result = dict()
+        for k, v in self.keybindings.items():
+            result[k] = {**v.output()}
+        with open(f'bindsets/{file_name}.json', 'w') as w:
+            w.write(json.dumps(result))
 
 
 class TabletKeyApp(App):
